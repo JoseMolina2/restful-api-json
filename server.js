5ec0b69f304793fd540440aab9c6dcbe0ea50efb
JoseMolina2 <jmmwork@protonmail.com>
2023-11-17 17:18:39 -0600

    feat: allow CORS

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
+const cors = require("cors");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(cors()); // Enable CORS
 // Add this before server.use(router)
 server.use(
   // Add custom route here if needed
